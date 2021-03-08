bd8d95c01353a5d0cf2f0153e4ac294879c7c3f9
lihini223 <lihininisansala95@gmail.com>
2021-03-09 00:12:18 +0530

    Create CombinedAttributesAdder class

--- fetch_data.py
+++ fetch_data.py
@@ -177,4 +177,24 @@ housing_cat_1hot = encoder.fit_transform(housing_cat_encoded.reshape(-1,1)) # ou
 encoder = LabelBinarizer()
 housing_cat_1hot = encoder.fit_transform(housing_cat)
 # print(housing_cat_1hot) gives a numpy array by default
+#custom transformers
+rooms_ix, bedrooms_ix, population_ix, household_ix = 3, 4, 5, 6
 
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True):
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        rooms_per_household = X[:, rooms_ix] / X[:, household_ix]
+        population_per_household = X[:, population_ix] / X[:, household_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household, bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+        
+        attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+        housing_extra_attribs = attr_adder.transform(housing.values)
